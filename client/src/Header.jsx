0833b2eea6da9752ddc25c8f216210b2d7e29efe
hypermarce <ayroutmarcelino@gmail.com>
2025-06-04 16:43:55 +1000

    Update font to Snell Roundhand, and enhance homepage layout

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,11 +5,11 @@ import logo from './assets/vic cam.png'; // Adjust the path as necessary
 export default function Header() {
     return(
         <header className="flex justify-between items-center p-4 text-black border-b border-black-600 bg-white">    
-        <div className="flex items-center">
+        <div className="">
             <img src={logo} alt="Logo" className="h-20 w-35 mr-2 ml-6" />
         </div>
-        <h1 style={{ fontFamily: "'Good Vibes', cursive" }} className="text-4xl font-bold">Vic Photography</h1>
-        <nav style={{ fontFamily: "'Good Vibes', cursive" }} className="mr-5 space-x-4 text-2xl">
+        <h1 style={{ fontFamily: "'Snell Roundhand', cursive" }} className="text-4xl font-bold ml-40">Vic Photography</h1>
+        <nav style={{ fontFamily: "'Snell Roundhand', cursive" }} className="mr-5 space-x-4 text-2xl">
             <Link to="/home" className="hover:underline">Home</Link>
             <Link to="/about" className="hover:underline ml-5">About Me</Link>
             <Link to="/enquire" className="hover:underline ml-5">Enquire</Link>
--- client/src/Home_page_content.jsx
+++ client/src/Home_page_content.jsx
@@ -5,29 +5,36 @@ import catholicmarriage from './assets/marriage in front of communion.webp';
 import baptism from './assets/baptism.webp';
 import HarbourBridge from './assets/harbour_bridge.png';
 import {Link} from 'react-router-dom';
-
-export default function HomePage() {
+import 'swiper/css';
+  
+  export default function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center bg-gray-100">
-                <img src={HarbourBridge} className="w-400 h-190"></img>
-                <div className="flex flex-row items-center justify-between bg-gray-50 p-4 rounded drop-shadow-lg mt-4 w-325">
-                    <div className="flex flex-col">
-                        <h1 className="text-2xl font-bold text-center -mt-30 mb-10">Welcome to My Website</h1>
-                        <p className="text-gray-700 text-center">Here you can find all the information you need to plan your perfect weddinga anfuhaeuhafhasoh.</p>
-                        <div className="flex justify-center mt-10">
-                        <Link to="/about" className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-black transition duration-300">About Me</Link>
-
-                        </div>
+            <img src={HarbourBridge} className="w-400 h-190" />
+            
+            <div className="flex flex-row items-center justify-between bg-gray-50 p-4 rounded drop-shadow-lg mt-4 w-325">
+                <div className="flex flex-col">
+                    <h1 className="text-2xl font-bold text-center -mt-30 mb-10 ml-11">Welcome to My Website</h1>
+                    <p className="text-gray-700 text-center ml-10">Here you can find all the information about me and my life as a photographer</p>
+                    <div className="flex justify-center mt-10">
+                        <Link to="/about" className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-black transition duration-300 ml-11">About Me</Link>
                     </div>
-                    <img src={catholicmarriage} className="w-150 h-100" alt="Catholic Marriage"></img>
                 </div>
-                
-            <div className="flex flex-wrap justify-center gap-4 p-4">    
-                <img src={baptism}className="w-150 h-100"></img>
-                <img src={wedding2}></img>
-                <img src={wedding}></img>
-                <img src={wedding}></img>
+                <img src={baptism} className="w-150 h-100" alt="Catholic Marriage" />
             </div>
+            <div className="flex flex-wrap justify-center gap-4 mt-4 mb-4">
+                <img src={wedding2} /> 
             </div>
+            <div className="flex flex-row items-center justify-between bg-gray-50 p-4 rounded drop-shadow-lg  mb-4 w-325">
+                <img src={catholicmarriage} className="w-150 h-100" alt="Catholic Marriage" />
+                <div className="flex flex-col">
+                    <h1 className="text-2xl font-bold text-center -mt-30 mb-10 mr-11">Create Memories</h1>
+                    <p className="text-gray-700 text-center mr-10">Here you can find all the information you need to plan your perfect wedding.</p>
+                    <div className="flex justify-center mt-10">
+                        <Link to="/enquire" className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-black transition duration-300 mr-11">Enquire Now</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
